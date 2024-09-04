54eedd6475f9c77432f114ff5ca801da5f668c23
Alexander Carlsson Musa <a.carlssons98@gmail.com>
2024-09-04 15:19:00 +0200

    Statiska metoder

--- WebsiteGeneratorOOP/WebsiteGeneratorOOP/Program.cs
+++ WebsiteGeneratorOOP/WebsiteGeneratorOOP/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace WebsiteGeneratorOOP
+﻿using System.Xml;
+
+namespace WebsiteGeneratorOOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ClassSpecificHtml("Klassen", 4);
+        }
+
+        static void OutputHtml()
+        {
+            Console.WriteLine("<!DOCTYPE html>");
+            Console.WriteLine("<html>");
+            Console.WriteLine("<body>");
+            Console.WriteLine("<h1>Välkomna!</h1>");
+            Console.WriteLine("<main>");
+            Console.WriteLine("<p>Kurs om C#</p>");
+            Console.WriteLine("<p>Kurs om Databaser</p>");
+            Console.WriteLine("</main>");
+            Console.WriteLine("</body>");
+            Console.WriteLine("</html>");
+        }
+
+        static void ClassSpecificHtml(string klassnamn, string klassmeddelande)
+        {
+            Console.WriteLine("<!DOCTYPE html>");
+            Console.WriteLine("<html>");
+            Console.WriteLine("<body>");
+            Console.WriteLine($"<h1>Välkomna {klassnamn}!</h1>");
+            Console.WriteLine($"<p><b>Meddelande:</b> {klassmeddelande}.</p>");
+            Console.WriteLine("<main>");
+            Console.WriteLine("<p>Kurs om C#</p>");
+            Console.WriteLine("<p>Kurs om Databaser</p>");
+            Console.WriteLine("</main>");
+            Console.WriteLine("</body>");
+            Console.WriteLine("</html>");
+        }
+
+        static void ClassSpecificHtml(string klassnamn, int antal)
+        {
+            Console.WriteLine("<!DOCTYPE html>");
+            Console.WriteLine("<html>");
+            Console.WriteLine("<body>");
+            Console.WriteLine($"<h1>Välkomna {klassnamn}!</h1>");
+            
+            for (int i = 0; i < antal; i++)
+            {
+                Console.Write($"Meddelande {i}: ");
+                Console.WriteLine($"<p><b>Meddelande:</b> {Console.ReadLine()}.</p>");
+            }
+            Console.WriteLine("<main>");
+            Console.WriteLine("<p>Kurs om C#</p>");
+            Console.WriteLine("<p>Kurs om Databaser</p>");
+            Console.WriteLine("</main>");
+            Console.WriteLine("</body>");
+            Console.WriteLine("</html>");
         }
     }
 }
