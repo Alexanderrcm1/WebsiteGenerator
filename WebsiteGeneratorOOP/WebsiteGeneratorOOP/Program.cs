7ccb30a319d8b5826f4c833f779f59dc0e656c7e
Alexander Carlsson Musa <a.carlssons98@gmail.com>
2024-09-04 14:13:44 +0200

    Added project

---
+++ WebsiteGeneratorOOP/WebsiteGeneratorOOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WebsiteGeneratorOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
